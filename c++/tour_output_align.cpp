89a8eb809d8ad7ac6f90dfd6b9b999cb8ea64490
Prakash Agarwal <prakash02dec@gmail.com>
2021-10-20 03:50:34 +0530

    field stream

--- c++/tour_output_align.cpp
+++ c++/tour_output_align.cpp
@@ -64,17 +64,29 @@ int main()
     };
 
     ruler();
+    const int  field_width {20} ;
+    const int  field_width1 {20} ;
+    const int  field_width2 {15} ;
+    const int  field_width3 {12} ;
+
     int len =70-tours.title.length();
     // std::cout<<std::setw(len/2)<<std::setfill('=');
     // Unformatted display so you can see how to access the vector elements
-    std::cout  << std::setw(len/2 +tours.title.length())<< std::setfill('=') << tours.title<< std::setw(len/2) <<"" << std::endl ;
+    std::cout  << std::setw(len/2 +tours.title.length())<< std::setfill('=') << tours.title<< std::setw(len/2 +1) <<"" << std::endl ;
+    
     std::cout << std::setfill(' ');
+
+    std::cout << std::left << std::setw(field_width) << "Country" << std::setw(field_width1) << "City" 
+    << std::setw(field_width2) << "Population" << std::setw(field_width3) << std::right << "Cost" << std::endl;
+    
+    std::cout << std::setfill('=') << std::setw(70) << "" << std::setfill(' ')  << std::endl;
+    
     for(auto country : tours.countries) {   // loop through the countries
-        std::cout << country.name << std::endl;
+        std::cout << std::setw(field_width) << std::left<< country.name << std::endl;
         for(auto city : country.cities) {       // loop through the cities for each country
-            std::cout << std::setw(10) << "" << std::left << std::setw(20) << city.name 
-                          << std::setw(25) << city.population 
-                          << std::setw(25) << city.cost 
+            std::cout << std::setw(field_width) << "" << std::left << std::setw(field_width1) << city.name 
+                          << std::setw(field_width2) << city.population 
+                          << std::setw(field_width3) << std::right << city.cost 
                           << std::endl;
         }
     }
