7b303578290937a8c52e59e68e3be623f5b75b19
Prakash Agarwal <prakash02dec@gmail.com>
2021-10-21 03:58:32 +0530

    file line numbering

--- c++/file_indentation/file_line_numbering.cpp
+++ c++/file_indentation/file_line_numbering.cpp
@@ -6,7 +6,19 @@ int main (){
 
     std::string line ;
     std::ifstream in_file {"romeoandjuliet.txt"} ;
-    std::ofstream out_file  {"romeoandjuliet.txt"} ;
+    std::ofstream out_file  {"romeoandjuliet_edited.txt"} ;
+
+    int i {1} ;
+    while(std::getline(in_file ,line)){
+        if(line.length() != 1)
+        out_file << std::setw(8) << std::left << i++ ;
+        out_file << line; 
+    }
+
+    in_file.close();
+    out_file.close();
+
+    return 0;
 
 
 }
