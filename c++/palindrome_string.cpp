ec318afdb088966f44e74c1c194393fce369bf99
Prakash Agarwal <prakash02dec@gmail.com>
2021-10-23 01:39:31 +0530

    palindrome string checker

---
+++ c++/palindrome_string.cpp
@@ -0,0 +1,37 @@
+// Section 20
+// Challenge 1
+// Identifying palindrome strings using a deque
+#include <cctype>
+#include <deque>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+bool is_palindrome(const std::string& s)
+{
+    std::deque<char> d1 , d2;
+    for(const auto &c : s){
+        if(std::isalpha(c)){ 
+        d1.emplace_back(std::toupper(c));
+        d2.emplace_front(std::toupper(c));
+        }
+    }
+    if(d1!=d2) return false ;
+    return true ;
+}
+
+int main()
+{
+    std::vector<std::string> test_strings{ "a", "aa", "aba", "abba", "abbcbba", "ab", "abc", "radar", "bob", "ana",
+        "avid diva", "Amore, Roma", "A Toyota's a toyota", "A Santa at NASA", "C++",
+        "A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!", "This is a palindrome", "palindrome" };
+   
+    std::cout << std::boolalpha;
+    std::cout << std::setw(8) << std::left << "Result" << "String" << std::endl;
+    for(const auto& s : test_strings) {
+        std::cout << std::setw(8) << std::left << is_palindrome(s)  << s << std::endl;
+    }
+    std::cout << std::endl;
+    return 0;
+}
